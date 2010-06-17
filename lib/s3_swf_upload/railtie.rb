6554990c36ecc9e5117d9ff478bb43343505b5c8
siong1987 <siong1987@gmail.com>
2010-06-18 00:03:41 +0800

    remove aws/s3 gem.

--- lib/s3_swf_upload/railtie.rb
+++ lib/s3_swf_upload/railtie.rb
@@ -8,10 +8,6 @@ module S3SwfUpload
       S3SwfUpload::S3Config.load_config
     end
     
-    rake_tasks do
-      load "s3_swf_upload/railties/tasks/s3_swf_upload.rake"
-    end
-    
     generators do
       require "s3_swf_upload/railties/generators/uploader/uploader_generator"
     end
